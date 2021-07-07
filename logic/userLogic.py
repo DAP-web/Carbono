0070830fda5a5209089ecc401f9cb5ec95daa903
Ángela Ruiz <ac.comayagua@yahoo.com>
2021-07-07 00:03:58 -0600

    holaaa, buenas, te dejo mi arduo trabajo

--- logic/userLogic.py
+++ logic/userLogic.py
@@ -4,29 +4,24 @@ class UserLogic(PybaLogic):
     def __init__(self):
         super().__init__()
 
-    def insertUser(self, username, email, password, salt):
+    def insertUser(self, username, email, password, salt, admin):
         database = self.databaseObj
         user = {
             "username":username,
             "useremail":email,
             "password":password,
-            "salt":salt
+            "salt":salt,
+            "admin": admin
         }
         sql = (
             f"""
-                INSERT INTO `sakila`.`user` (`user_name`, `user_email`, `password`, `salt`)
-                VALUES
-                ('{user["username"]}', '{user["useremail"]}', '{user["password"]}', '{user["salt"]}');
+            INSERT INTO `carbonodb`.`user` (`username`, `email`, `password`,`salt`, `admin`) 
+            VALUES ('{user["username"]}', '{user["useremail"]}m', '{user["password"]}', '{user["salt"]}',
+            '{user["admin"]}' ;
+            
             """
         )
         rows = database.executeNonQueryRows(sql)
         return rows
 
-    def getUserByUsername(self, username):
-        database = self.databaseObj
-        sql = f"SELECT user_name, password, salt FROM sakila.user WHERE user_name = '{username}';"
-        result = database.executeQuery(sql)
-        if len(result) > 0:
-            return result[0]
-        else:
-            return []
+    
