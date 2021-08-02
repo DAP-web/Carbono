304ab1a986d179012aa3572fead6da8573fbec54
marcelamata <65987934+marcelamata@users.noreply.github.com>
2021-08-01 19:53:19 -0600

    user con logica

---
+++ logic/userLogic.py
@@ -0,0 +1,54 @@
+from core.pyba_logic import PybaLogic
+
+class UserLogic(PybaLogic):
+    def __init__(self):
+        super().__init__()
+
+    def insertUser(self, username, email, password, salt, admin):
+        database = self.databaseObj
+        user = {
+            "username":username,
+            "useremail":email,
+            "password":password,
+            "salt":salt,
+            "admin": admin
+        }
+        sql = (
+            f"""
+            INSERT INTO `carbonodb`.`user` (`username`, `email`, `password`,`salt`, `admin`) 
+            VALUES ('{user["username"]}', '{user["useremail"]}', '{user["password"]}', '{user["salt"]}',
+            '{user["admin"]}');
+            """
+        )
+        rows = database.executeNonQueryRows(sql)
+        return rows
+
+    def getUserByEmail(self, email):
+        database = self.databaseObj
+        sql = (f"""
+            SELECT userid, username, email, password, salt, admin 
+            FROM carbonodb.user where `email` like '{email}';
+            """)
+        result = database.executeQuery(sql)
+        if len(result) > 0:
+            return result[0]
+        else:
+            return []
+
+    def getUsersClients(self):
+        database = self.databaseObj
+        sql = (f"SELECT * FROM carbonodb.user where admin = 0;")
+        result = database.executeQuery(sql)
+        if len(result) > 0:
+            return result
+        else:
+            return []
+
+    def deleteUserClient(self, id):
+        database = self.databaseObj
+        sql = (f"""DELETE FROM `carbonodb`.`user` WHERE userid={id};""")
+        rows = database.executeNonQueryRows(sql)
+        return rows
+
+
+    
