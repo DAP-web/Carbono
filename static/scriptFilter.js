d1e35257984572afeb0d4bdadf047f62eece601d
Diego Portillo <65975109+DAP-web@users.noreply.github.com>
2021-08-06 09:47:30 -0600

    Filters part one

---
+++ static/scriptFilter.js
@@ -0,0 +1,16 @@
+function cambioDecision() {
+
+    if (document.getElementById("updateBTN").style.display == "block") {
+        document.getElementById("updateBTN").style.display = "none";
+        document.getElementById("deleteBTN").style.display = "none";
+        document.getElementById("filtersInputs").style.display = "block";
+        document.getElementById("formUPD-FIL").action = "{{url_for('filterTasks')}}";
+        document.getElementById("decisionboton").innerHTML = "No filtrar tareas";
+    } else {
+        document.getElementById("updateBTN").style.display = "block";
+        document.getElementById("deleteBTN").style.display = "block";
+        document.getElementById("filtersInputs").style.display = "none";
+        document.getElementById("formUPD-FIL").action = "{{url_for('checkTask')}}";
+        document.getElementById("decisionboton").innerHTML = "Filtrar tareas";
+    }
+}
